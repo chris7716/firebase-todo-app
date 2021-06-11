6ee0ff7d4ab5cf0ba4d19e93b17a21deeed87fb3
Hasitha Kaushan <hkraclh@gmail.com>
2021-06-11 22:19:34 +0530

    Remove unwanted lines

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,10 +26,3 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const api = functions.https.onRequest(app);
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
